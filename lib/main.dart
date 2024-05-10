6e61500c0dd132308b258e1d48341be12436c5b5
Nico Herlim <nicoherlim2003@gmail.com>
2024-05-11 00:04:12 +0700

    login langsung masuk ketika ada token

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
             useMaterial3: true,
           ),
-          home: SplashScreen(),
+          home: SplashScreen(token: token!),
         );
       },
     );
--- lib/pages/splashScreen.dart
+++ lib/pages/splashScreen.dart
@@ -1,20 +1,22 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:p3l_atmabakery/pages/homeNavbar.dart';
 import 'package:p3l_atmabakery/pages/welcomePage.dart';
 
 class SplashScreen extends StatefulWidget {
+  final String token;
+  const SplashScreen({this.token = "", Key? key}) : super(key: key);
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  int _countdown = 5;
-
-  List<String> _countdownStrings = ['0 Detik','1 Detik', '2 Detik', '3 Detik', '4 Detik', '5 Detik'];
-
+  int _countdown = 3;
+  String token = "";
   @override
   void initState() {
     super.initState();
+    token = widget.token;
     Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {
         if (_countdown > 0) {
@@ -22,9 +24,11 @@ class _SplashScreenState extends State<SplashScreen> {
         } else {
           timer.cancel();
           Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => WelcomePage()),
-          );
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    token.isEmpty ? WelcomePage() : HomeNavbar(),
+              ));
         }
       });
     });
@@ -46,26 +50,6 @@ class _SplashScreenState extends State<SplashScreen> {
                   height: 500,
                 ),
               ],
-            ),
-          ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 255, 255, 255),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                _countdownStrings[_countdown],
-                style: TextStyle(
-                  color: const Color.fromRGBO(244, 142, 40, 1),
-                  fontSize: 14,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Poppins',
-                ),
-              ),
             ),
           ),
         ],
